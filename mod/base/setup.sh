2d6e43193d074967b615131f588b1ccfbd55f873
chandi <git@chandi.it>
2020-04-09 19:33:13 +0200

    rearrange base files

--- mod/base/setup.sh
+++ mod/base/setup.sh
@@ -9,12 +9,6 @@ TURN_XML=$SERVLET_DIR/WEB-INF/classes/spring/turn-stun-servers.xml
 
 while [ ! -f $SERVLET_DIR/WEB-INF/classes/bigbluebutton.properties ]; do sleep 1; echo -n '.'; done
 
-
-# provide dummy nginx config
-# so bbb scripts are happy, even if we don't use nginx here
-echo -e "pid /run/nginx.pid;\n events {}" > /etc/nginx/nginx.conf
-systemctl restart nginx
-
 # delete IPv6 sip profiles
 rm -rf /opt/freeswitch/conf/sip_profiles/*-ipv6*
 
@@ -23,7 +17,7 @@ if [ -f /var/www/bigbluebutton/client/conf/config.xml ]; then
   sed -i 's/offerWebRTC="false"/offerWebRTC="true"/g' /var/www/bigbluebutton/client/conf/config.xml
 fi
 
-while [ ! -f /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp ]; do sleep 1; echo -n '.'; done
+# while [ ! -f /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp ]; do sleep 1; echo -n '.'; done
 
 
 if [ -f /var/www/bigbluebutton/client/conf/config.xml ]; then
@@ -41,9 +35,9 @@ fi
 yq w -i /usr/local/bigbluebutton/core/scripts/bigbluebutton.yml playback_protocol https
 chmod 644 /usr/local/bigbluebutton/core/scripts/bigbluebutton.yml 
 
-if [ -f /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp ]; then
-  sed -i 's|String BigBlueButtonURL = ".*|String BigBlueButtonURL = "http://127.0.0.1:8090/bigbluebutton/";|g' /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp
-fi
+# if [ -f /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp ]; then
+#   sed -i 's|String BigBlueButtonURL = ".*|String BigBlueButtonURL = "http://127.0.0.1:8090/bigbluebutton/";|g' /var/lib/$TOMCAT_USER/webapps/demo/bbb_api_conf.jsp
+# fi
 
 # Update HTML5 client (if installed) to use SSL
 if [ -f  /usr/share/meteor/bundle/programs/server/assets/app/config/settings.json ]; then
@@ -52,7 +46,18 @@ if [ -f  /usr/share/meteor/bundle/programs/server/assets/app/config/settings.jso
 fi
 
 bbb-conf --setip $HOST
-
+# Assigning meet.livingutopia.org for testing for firewall in /var/www/bigbluebutton/client/conf/config.xml
+# Assigning meet.livingutopia.org for rtmp:// in /var/www/bigbluebutton/client/conf/config.xml
+# Assigning meet.livingutopia.org for servername in /etc/nginx/sites-available/bigbluebutton
+# Assigning meet.livingutopia.org for http[s]:// in /var/www/bigbluebutton/client/conf/config.xml
+# Assigning meet.livingutopia.org for publishURI in /var/www/bigbluebutton/client/conf/config.xml
+# Assigning meet.livingutopia.org for web application URL in /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+# sed: cannot rename /usr/share/bbb-web/WEB-INF/classes/sed3ZgbUO: Device or resource busy
+# sed: cannot rename /usr/share/bbb-web/WEB-INF/classes/sednkGxuN: Device or resource busy
+# Assigning meet.livingutopia.org for web application URL in /usr/share/bbb-apps-akka/conf/application.conf 
+# Assigning meet.livingutopia.org for api demos in /var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
+# Assigning meet.livingutopia.org for record and playback in /usr/local/bigbluebutton/core/scripts/bigbluebutton.yml
+# Assigning meet.livingutopia.org for playback of recordings:         
 
 # Disable auto start 
 find /etc/systemd/ | grep wants | xargs -r -n 1 basename | grep service | grep -v networking | grep -v tty   | xargs -r -n 1 -I __ systemctl disable __
