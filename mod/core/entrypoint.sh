a3027a036b8ac4fa6860ac9f43de2e38c5b756ba
chandi <git@chandi.it>
2020-04-19 18:52:48 +0200

    generate missing recording folder structure.

--- mod/core/entrypoint.sh
+++ mod/core/entrypoint.sh
@@ -3,6 +3,19 @@
 # print logs to stdout/stderr as soon as systemd is started
 sh -c 'sleep 5 && journalctl -f' &
 
+# generate bbb folders
+mkdir -p /var/bigbluebutton/recording/raw
+mkdir -p /var/bigbluebutton/recording/process
+mkdir -p /var/bigbluebutton/recording/publish
+mkdir -p /var/bigbluebutton/recording/status/recorded
+mkdir -p /var/bigbluebutton/recording/status/archived
+mkdir -p /var/bigbluebutton/recording/status/processed
+mkdir -p /var/bigbluebutton/recording/status/sanity
+mkdir -p /var/bigbluebutton/published
+mkdir -p /var/bigbluebutton/deleted
+mkdir -p /var/bigbluebutton/unpublished
+
+
 dockerize \
     -template /opt/freeswitch/conf/vars.xml.tmpl:/opt/freeswitch/conf/vars.xml \
     -template /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties.tmpl:/usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties \
