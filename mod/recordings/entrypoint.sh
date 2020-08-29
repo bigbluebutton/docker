cd76d89387d5065a239cd71bbdda7679ea0fe051
Sofyan Sugianto <sofyanartem@gmail.com>
2020-08-29 21:05:39 +0700

    fix recording entrypoint error

--- mod/recordings/entrypoint.sh
+++ mod/recordings/entrypoint.sh
@@ -7,20 +7,20 @@ cd /usr/local/bigbluebutton/core/scripts
 PATH_CHECK="/var/bigbluebutton/recording/status"
 while true; do
     echo "execute workers..."
-    if [[ compgen -G "$PATH_CHECK/recorded/*.done" ]];then
+    if [[ $(compgen -G "$PATH_CHECK/recorded/*.done") ]];then
     bundle exec ruby rap-archive-worker.rb
     fi
-    if [[ compgen -G "$PATH_CHECK/archived/*.done" ]];then
+    if [[ $(compgen -G "$PATH_CHECK/archived/*.done") ]];then
     bundle exec ruby rap-sanity-worker.rb
     fi
-    if [[ compgen -G "$PATH_CHECK/sanity/*.done" ]];then
+    if [[ $(compgen -G "$PATH_CHECK/sanity/*.done") ]];then
     bundle exec ruby rap-process-worker.rb
     fi
-    if [[ compgen -G "$PATH_CHECK/processed/*.done" ]];then
+    if [[ $(compgen -G "$PATH_CHECK/processed/*.done") ]];then
     bundle exec ruby rap-publish-worker.rb
     fi
     #bundle exec ruby rap-caption-inbox.rb
-    if [[ compgen -G "$PATH_CHECK/ended/*.done" ]];then
+    if [[ $(compgen -G "$PATH_CHECK/ended/*.done") ]];then
     bundle exec ruby rap-events-worker.rb
     fi
     sleep 30s
