7add88578ef52b7ae4a74c2b96590c23fed97267
chandi <git@chandi.it>
2020-09-06 22:02:22 +0200

    v2.3: dockerized libreoffice

--- mod/core/web-run.sh
+++ mod/core/web-run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# forward libreoffice ports to this container
+for i in `seq 1 4` ; do
+    let PORT=8200+${i}
+    echo "forward port $PORT to the libreoffice container"
+    socat TCP-LISTEN:$PORT,fork TCP:10.7.7.7:$PORT &
+done
+
 java -Dgrails.env=prod -Dserver.address=0.0.0.0 -Dserver.port=8090 -Xms384m -Xmx384m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/bigbluebutton/diagnostics -cp WEB-INF/lib/*:/:WEB-INF/classes/:. org.springframework.boot.loader.WarLauncher
 
 
