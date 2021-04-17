aedfd20b8e56a6546143eb8cb05b84a758f20e52
chandi <git@chandi.it>
2021-04-17 18:23:19 +0200

    v2.3-beta-4

--- mod/bbb-web/office-convert.sh
+++ mod/bbb-web/office-convert.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# This script receives two params
+# This script receives three params
 # Param 1: Input office file path (e.g. "/tmp/test.odt")
 # Param 2: Output pdf file path (e.g. "/tmp/test.pdf")
+# Param 3: Destination Format (pdf default)
+#If output format is missing, define PDF
+convertTo="${3:-pdf}"
 
-curl -v -X POST "http://jodconverter:8080/lool/convert-to/pdf" \
+curl -v -X POST "http://jodconverter:8080/lool/convert-to/$convertTo" \
     -H "accept: application/octet-stream" \
     -H "Content-Type: multipart/form-data" \
     -F "data=@$1" > $2
