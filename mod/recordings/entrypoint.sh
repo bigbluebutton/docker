79734af55317040df92e9a094ab0304959f26d76
chandi <git@chandi.it>
2021-11-27 17:49:42 +0100

    minor fixes for recording scripts

--- mod/recordings/entrypoint.sh
+++ mod/recordings/entrypoint.sh
@@ -7,5 +7,6 @@ chown -R bigbluebutton:bigbluebutton /var/log/bigbluebutton
 
 dockerize \
     -template /usr/local/bigbluebutton/core/scripts/bigbluebutton.yml.tmpl:/usr/local/bigbluebutton/core/scripts/bigbluebutton.yml \
+    -template /etc/bigbluebutton/bbb-web.properties.tmpl:/etc/bigbluebutton/bbb-web.properties \
     -stdout /var/log/bigbluebutton/recording.log \
     /usr/bin/supervisord --nodaemon
