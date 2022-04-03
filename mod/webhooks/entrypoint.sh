7eb142b43ee60f1414b67ad294d74818d0071568
chandi <git@chandi.it>
2022-04-03 23:24:20 +0200

    webhooks: avoid including whole config file

--- mod/webhooks/entrypoint.sh
+++ mod/webhooks/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+set -e
+TARGET=/bbb-webhooks/config/production.yml
+cp /bbb-webhooks/config/default.example.yml $TARGET
+
+yq e -i ".bbb.sharedSecret = \"$SHARED_SECRET\"" $TARGET
+yq e -i ".bbb.serverDomain = \"$DOMAIN\"" $TARGET
+yq e -i ".bbb.auth2_0 = true" $TARGET
+yq e -i ".server.bind = \"0.0.0.0\"" $TARGET
+yq e -i ".hooks.getRaw = false" $TARGET
+yq e -i ".redis.host = \"redis\"" $TARGET
 
 export NODE_ENV=production
-cd /app
-dockerize \
-    -wait tcp://redis:6379 \
-    -template /app/config/default.yml.tmpl:/app/config/default.yml \
-    node app.js
+
+cd /bbb-webhooks
+node app.js
 
